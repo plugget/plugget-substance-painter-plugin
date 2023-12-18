e9de76b6f4bf6d7de0fca51bbf84d413d3cb7a0d
Hannes Delbeke <hannesdelbeke@gmail.com>
2023-12-18 17:55:13 +0000

    Update __init__.py

--- plugget_substance_painter_plugin/__init__.py
+++ plugget_substance_painter_plugin/__init__.py
@@ -7,18 +7,6 @@ substance_ui_elements = []
 widget = None
 
 
-# hello world widget
-class HelloWorld(QtWidgets.QWidget):
-    def __init__(self, parent, *args, **kwargs):
-        super(HelloWorld, self).__init__(parent, *args, **kwargs)
-        # set title
-        self.setWindowTitle("Hello World")
-        # self.setWindowIcon(QtGui.QIcon(":/SP_ICON_16x16"))  # TODO this does not work
-        self.setWindowFlags(self.windowFlags() | QtCore.Qt.Window)
-        self.setLayout(QtWidgets.QVBoxLayout())
-        self.layout().addWidget(QtWidgets.QLabel("Hello World!"))
-
-
 def show_widget():
     import plugget_qt
     global widget
@@ -29,11 +17,8 @@ def show_widget():
 
 
 def start_plugin():
-    # Create a text widget for a menu
-    Action = QtWidgets.QAction("Hello", triggered=show_widget)
-    # Add this widget to the existing Window menu of the application
-    substance_painter.ui.add_action(substance_painter.ui.ApplicationMenu.Window, Action)
-    # Store the widget for proper cleanup later when stopping the plugin
+    action = QtWidgets.QAction("Plugget", triggered=show_widget)
+    substance_painter.ui.add_action(substance_painter.ui.ApplicationMenu.Window, action)
     substance_ui_elements.append(Action)
 
 
